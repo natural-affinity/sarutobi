d6d54e437c4aa24e22aae9c8c42d98197afe69aa
Rizwan Tejpar <natural-affinity@users.noreply.github.com>
2019-05-09 00:35:11 -0700

    initial version: cli interface

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	docopt "github.com/docopt/docopt-go"
+)
+
+// Version identifier
+const Version = "0.0.1"
+
+// Usage message (docopt interface)
+const Usage = `
+  Sarutobi
+    Inspirational quotes
+
+  Usage:
+    sarutobi [--author a | --tag t] <file>
+    sarutobi --help
+    sarutobi --version
+
+  Options:
+    -h, --help        display help information
+    -v, --version     display version information
+    -t, --tag t       limit relevant quotes by tag
+    -a, --author a    limit relevant quotes by author
+`
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	// parse usage string and collect args
+	args, err := docopt.ParseArgs(Usage, os.Args[1:], Version)
+	if err != nil {
+		log.Fatalf("invalid usage string: %s", err.Error())
+	}
+
+	// extract options and args
+	tag, _ := args.String("--tag")
+	author, _ := args.String("--author")
+	research, err := ioutil.ReadFile(args["<file>"].(string))
+	if err != nil {
+		log.Fatalf("invalid file: %s", err.Error())
+	}
+
+	fmt.Printf("%s_%s", tag, author, research)
+}
